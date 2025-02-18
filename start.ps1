bd1c9ccfa2e988d9546304c6dd3bd11c90cdf8ab
jk2804 <joni.kaibel@gmail.com>
2025-02-18 17:13:03 +0100

    Start File

---
+++ start.ps1
@@ -0,0 +1,31 @@
+Write-Host "Starte Container..."
+docker compose up --build -d
+
+Write-Host "Warte auf Container-Start (10 Sekunden)..."
+Start-Sleep -Seconds 10
+
+Write-Host "Öffne Dashboard..."
+Start-Process "http://localhost:8080"
+
+Write-Host "Druecke 'Q' zum Beenden der Container"
+Write-Host "Druecke 'X' zum Beenden des Scripts"
+
+while ($true) {
+    if ([Console]::KeyAvailable) {
+        $key = [Console]::ReadKey($true)
+        
+        switch ($key.Key) {
+            'Q' {
+                Write-Host "`nBeende Container..."
+                docker compose down
+                break
+            }
+
+            'X' {
+                Write-Host "`nBeende Script..."
+                exit
+            }
+        }
+    }
+    Start-Sleep -Milliseconds 100
+}
